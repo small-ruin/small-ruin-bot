6ab989df9a97bfa2636e504e3cbefa47d20ca844
hilshire <hillshire.triela@gmail.com>
2021-08-08 18:39:42 +0800

    update help

--- src/constant.ts
+++ src/constant.ts
@@ -6,8 +6,17 @@ export const WITHDRAWAL_CLOSE_ALERT =
     尽归尘土...`
 export const HELP =
 `QQ群防撤回机器人开发版本 v0.0.1
+ sm 指 small-ruin，小废墟
 Usage:
   @<bot> on - 打开防撤回功能
   @<bot> off - 关闭防撤回功能
   @<bot> status - 查看机器人状态
-  @<bot> help - 帮助`
+  @<bot> help - 帮助
+  sm list 战役列表
+  sm list <adventure name> 目标战役的log列表
+    -u, --url 显示战役或者log的url
+    -l, --limit 显示log的熟练，默认10条，对战役无效
+    -e, --latest 显示最新的log，对战役无效
+  sm search <adventure name> <keyword> 搜索带有关键词的log
+`
+  
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app
 .plugin(SmallRuin)
 .start({
   wss: false, // 是否使用wss
-  accessToken: "small-ruin-bot-1", // API的访问token
+  accessToken: "", // API的访问token
   host: '127.0.0.1', // 客户端地址
   port: 6700, // 客服端端口
   reconnection: true, // 是否连线错误时自动重连
